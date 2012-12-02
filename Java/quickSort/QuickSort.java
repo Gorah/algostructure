6b28ecb8fbef2b15447fa8bb2c5d0483d789333a
Gorah <elon.aep.delin@gmail.com>
2012-12-02 20:01:50 +0100

    work in progress with quick sort

--- Java/quickSort/QuickSort.java
+++ Java/quickSort/QuickSort.java
@@ -1,13 +1,33 @@
 package quickSort;
 
+import java.util.ArrayList;
+
+
+
 public class QuickSort {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void quickS(ArrayList<Integer> arr){
+		int start=0, middle, last;
+		Integer val;
+		
+		last = arr.size() -1;
+		middle = (start + last)/2;
+		
+		val = arr.get(middle);
+		arr.remove(middle);
+		
+		
+		
 	}
-
+	
+	public static void main(String[] args) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		arr.add(3);
+		arr.add(1);
+		arr.add(4);
+		arr.add(5);
+		arr.add(7);
+		arr.add(2);
+		quickS(arr);
+	}
 }
